5cc0371f678d7d6cfd5b3df1371d4c7031a7d0c4
Luzía <luciaglsn@correo.ugr.es>
2023-11-10 17:27:28 +0100

    Modela la gestión de las tareas #16
    Se almacenan las tareas en dos vectores, uno de tareas completadas y otro de tareas disponibles. La idea es usarlos en la asignación de las tareas. Se crea también un método para establecer el número de puntos que cada compañero tiene que alcanzar semanalmente, teniendo en cuenta que se ha orientado el problema de manera en que el reparto se hace en función del tiempo y la dificultad, a modo de conseguir un reparto equitativo closes marinajcs#16

--- lib/reparto.ts
+++ lib/reparto.ts
@@ -1,5 +1,38 @@
+import { Compi } from './compi';
+
 type Tarea = {
     nombre: string;
     duracionEstimada: number;
     puntuacion: number;
+}
+
+class Reparto {
+
+    constructor(
+        readonly compis: Array<Compi>,
+        readonly tareasDisponibles: Array<Tarea>,
+        readonly tareasCompletadas: Array<Tarea>,
+        public goal: number
+    ){}
+
+    setGoal(): void{
+        let totalPuntuacion = 0;
+        for (const tarea of this.tareasDisponibles) {
+            totalPuntuacion += tarea.puntuacion;
+        }
+        this.goal = totalPuntuacion/this.compis.length;
+    }
+
+    asignarTareas(): void{
+        //Dividir las tareas entre los compañeros, según sus horas disponibles
+
+        //Debería empezar a asignar por el compi que tenga menos horas disponibles,
+        //para poder tener todas las combinaciones posibles para alcanzar el goal
+    }
+
+    reiniciarSemana(): void{
+        //Volcar el vector de tareas completadas en el de tareas disponibles
+    }
+
+
 }
